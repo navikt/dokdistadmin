b88736bacaebd1efc229d7f567a23777f632e54c
tsigab <tsigab.angosom@gmail.com>
2022-12-02 15:14:24 +0100

    MMA-6281 oppryddet azure og la til oracle dependencies

--- core/src/main/java/no/nav/dokdistadmin/CoreConfig.java
+++ core/src/main/java/no/nav/dokdistadmin/CoreConfig.java
@@ -1,6 +1,5 @@
 package no.nav.dokdistadmin;
 
-import no.nav.dokdistadmin.azure.Azureproperties;
 import no.nav.dokdistadmin.config.DokdistadminProperties;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties({
-		Azureproperties.class,
 		DokdistadminProperties.class
 })
 @EnableJwtTokenValidation(ignore = {"org.springframework", "org.springdoc"})
