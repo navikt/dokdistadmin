91ac86bfb7877dfabb865754becdc2272fd41125
tsigab <tsigab.angosom@gmail.com>
2022-12-05 10:06:57 +0100

    MMA-6281 refaktorert og la til interceptor

---
+++ core/src/main/java/no/nav/dokdistadmin/domain/ChangeStamp.java
@@ -0,0 +1,101 @@
+package no.nav.dokdistadmin.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.time.LocalDateTime;
+
+@Builder
+@Embeddable
+@AllArgsConstructor
+public class ChangeStamp {
+
+	private static final long serialVersionUID = 61541164562562288L;
+
+	@Column(name = "opprettet_av", nullable = false, updatable = false)
+	private String opprettetAv;
+
+	@Column(name = "opprettet_dato", nullable = false, updatable = false)
+	private LocalDateTime opprettetDato;
+
+	@Column(name = "endret_av")
+	private String endretAv;
+
+	@Column(name = "endret_dato")
+	private LocalDateTime endretDato;
+
+	/**
+	 * Constructs a new ChangeStamp. The constructor should only be called once, when the object embedding this
+	 * <code>ChangeStamp</code> object is actually created for the first time.
+	 *
+	 * @param userId the user id that creates object embedding this <code>ChangeStamp</code> object
+	 */
+	public ChangeStamp(String userId) {
+		this.opprettetAv = userId;
+		opprettetDato = LocalDateTime.now();
+	}
+
+	/**
+	 * No-arg constructor should only be used by persistence provider.
+	 * The application should use the parameterized constructor.
+	 */
+	protected ChangeStamp() {
+	}
+
+	/**
+	 * Method called whenever the object embedding this <code>ChangeStamp</code> object has been updated.
+	 *
+	 * @param userId user id that made the update
+	 */
+	public void updatedBy(String userId) {
+		endretAv = userId;
+		endretDato = LocalDateTime.now();
+	}
+
+	/**
+	 * Getter for the opprettetAv property.
+	 *
+	 * @return the opprettetAv
+	 */
+	public String getOpprettetAv() {
+		return opprettetAv;
+	}
+
+	/**
+	 * Getter for the opprettetDato property.
+	 *
+	 * @return the opprettetDato
+	 */
+	public LocalDateTime getOpprettetDato() {
+		return opprettetDato;
+	}
+
+	/**
+	 * Getter for the endretAv property.
+	 *
+	 * @return the endretAv
+	 */
+	public String getEndretAv() {
+		return endretAv;
+	}
+
+	/**
+	 * Getter for the endretDato property.
+	 *
+	 * @return the endretDato
+	 */
+	public LocalDateTime getEndretDato() {
+		return endretDato;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return "ChangeStamp [opprettetAv=" + opprettetAv + ", opprettetDato=" + opprettetDato + ", endretAv=" + endretAv
+				+ ", endretDato=" + endretDato + "]";
+	}
+}
---
+++ core/src/main/java/no/nav/dokdistadmin/utils/MDCConstants.java
@@ -0,0 +1,12 @@
+package no.nav.dokdistadmin.utils;
+
+public class MDCConstants {
+	public static final String CALL_ID = "callId";
+	public static final String REQUEST_ID = "requestId";
+	public static final String USER_ID = "userId";
+	public static final String CONSUMER_ID = "consumerId";
+	public static final String NAV_CALLID = "Nav-Callid";
+
+	private MDCConstants() {
+	}
+}
