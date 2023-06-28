02df96ac6b48f9a84b1495660a2b3c3c00b98ac4
tsigab <tsigab.angosom@gmail.com>
2023-06-28 13:25:36 +0200

    MMA-6647 fikset db-script mappe

--- core/src/main/resources/db.migration.DokdistDS/V1.10__kodeverk_v105.sql
+++
@@ -1,4 +0,0 @@
--- Dokumentdistribusjon_Kodeverk-105
-insert into K_BREV_PROD_APP (k_brev_prod_app,dekode,opprettet_dato,opprettet_av,endret_dato,endret_av) VALUES ('AVG_REP','AVG_REP',timestamp '2014-02-07 10:00:00','T. Bjørge',NULL,NULL);
-insert into K_BREV_PROD_APP (k_brev_prod_app,dekode,opprettet_dato,opprettet_av,endret_dato,endret_av) VALUES ('IT_PREMIE','IT_PREMIE',timestamp '2014-02-07 10:00:00','T. Bjørge',NULL,NULL);
-insert into K_BREV_PROD_APP (k_brev_prod_app,dekode,opprettet_dato,opprettet_av,endret_dato,endret_av) VALUES ('UR_LT','UR_LT',timestamp '2014-02-07 10:00:00','T. Bjørge',NULL,NULL);
