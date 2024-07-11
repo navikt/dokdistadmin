99cef8011f1b61f95dca905757c7fe586667208c
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2024-07-11 11:18:21 +0200

    MMA-7512 Oppsett av fcf/ons for databasen
    * Forberedelse til cloud@customer flytt

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env sh
 
-if test -f /var/run/secrets/nais.io/dokdistDB/username;
+if test -f /var/run/secrets/nais.io/db_creds/username;
 then
     echo "Setting SPRING_DATASOURCE_USERNAME"
-    export SPRING_DATASOURCE_USERNAME=$(cat /var/run/secrets/nais.io/dokdistDB/username)
+    export SPRING_DATASOURCE_USERNAME=$(cat /var/run/secrets/nais.io/db_creds/username)
 fi
 
-if test -f /var/run/secrets/nais.io/dokdistDB/password;
+if test -f /var/run/secrets/nais.io/db_creds/password;
 then
     echo "Setting SPRING_DATASOURCE_PASSWORD"
-    export SPRING_DATASOURCE_PASSWORD=$(cat /var/run/secrets/nais.io/dokdistDB/password)
+    export SPRING_DATASOURCE_PASSWORD=$(cat /var/run/secrets/nais.io/db_creds/password)
+fi
+
+if test -f /var/run/secrets/nais.io/db_config/jdbc_url;
+then
+    export SPRING_DATASOURCE_URL=$(cat /var/run/secrets/nais.io/db_config/jdbc_url)
+    echo "Setting SPRING_DATASOURCE_URL=$SPRING_DATASOURCE_URL"
+fi
+
+if test -f /var/run/secrets/nais.io/db_config/ons_host;
+then
+    export DOKDISTADMIN_DATABASE_ONSHOSTS=$(cat /var/run/secrets/nais.io/db_config/ons_host)
+    echo "Setting DOKDISTADMIN_DATABASE_ONSHOSTS=$DATABASE_ONSHOSTS"
 fi
 
 echo "Exporting appdynamics environment variables"
